995fc13ba01659bd9a495f35502585401b294e86
Tanishq Saluja <tanishqsaluja18@gmail.com>
2018-10-26 01:04:56 +0530

    commit : added text to speech for the generated text

--- app/src/main/java/mlexpert/tanishqsaluja/fireblast/MainActivity.java
+++ app/src/main/java/mlexpert/tanishqsaluja/fireblast/MainActivity.java
@@ -4,55 +4,110 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.util.Locale;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
-    private VideoView videoView;
+    private static final int PICK_VIDEO_REQUEST = 123;
     private Button buttonChoose, buttonUpload;
-    private static final int PICK_IMAGE_REQUEST = 123;
+    private TextView textView;
+    //private EditText editText;
     private Uri filepath;
     private StorageReference mStorageRef;
     private MediaController mediaController;
     private MyVideoView myVideoView;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         buttonChoose = findViewById(R.id.chooser);
         buttonUpload = findViewById(R.id.upload);
-        // videoView = findViewById(R.id.video);
-        myVideoView=findViewById(R.id.myvideo);
-        myVideoView.setVideoSize(600,400);
+        myVideoView = findViewById(R.id.myvideo);
+        textView = findViewById(R.id.text);
+        //editText = findViewById(R.id.text);
+
+        myVideoView.setVideoSize(350, 250);
         mediaController = new MediaController(this);
+
         mStorageRef = FirebaseStorage.getInstance().getReference();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("users");
+
         buttonChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setType("video/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent.createChooser(intent, "Select a video"), PICK_IMAGE_REQUEST);
+                startActivityForResult(intent.createChooser(intent, "Select a video"), PICK_VIDEO_REQUEST);
             }
         });
+
         buttonUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 upload();
+            }
+        });
+
+        // Real-time [ No need to make a separate background thread for it as it is real-time]
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, String> map = (Map<String, String>) dataSnapshot.getValue();
+                final String text = map.get("generated_text");
+                textView.setText(map.get("generated_text"));
+                textToSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if (status == TextToSpeech.SUCCESS) {
+                            // Make a multi-language model
+                            int result = textToSpeech.setLanguage(Locale.ENGLISH);
+                            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("error", "This Language is not supported");
+                            } else {
+                                textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+                            }
+                        } else {
+                            Log.e("error", "Initilization Failed!");
+                        }
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+                Log.d("TAG: ", "Failed to read value: ", databaseError.toException());
             }
         });
     }
@@ -60,10 +115,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+        if (requestCode == PICK_VIDEO_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             filepath = data.getData();
             try {
-                myVideoView.setVideoSize(800, 600);
+                myVideoView.setVideoSize(350, 250);
                 myVideoView.setMediaController(mediaController);
                 mediaController.setAnchorView(myVideoView);
                 myVideoView.setVideoURI(filepath);
@@ -108,7 +163,6 @@ public class MainActivity extends AppCompatActivity {
                     });
         } else {
             Toast.makeText(getApplicationContext(), "File not selected", Toast.LENGTH_SHORT).show();
-
         }
     }
 }
